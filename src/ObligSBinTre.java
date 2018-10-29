a5ee8af683bb7ebaad618f66b93cabbeee123c0c
JacobNicLarsen <JacobNicLarse@gmail.com>
2018-10-29 23:18:41 +0100

    first commit

---
+++ src/ObligSBinTre.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+
+public class ObligSBinTre<T> implements Beholder<T> {
+
+
+    @Override
+    public boolean leggInn(T verdi) {
+        return false;
+    }
+
+    @Override
+    public boolean inneholder(T verdi) {
+        return false;
+    }
+
+    @Override
+    public boolean fjern(T verdi) {
+        return false;
+    }
+
+    @Override
+    public int antall() {
+        return 0;
+    }
+
+    @Override
+    public boolean tom() {
+        return false;
+    }
+
+    @Override
+    public void nullstill() {
+
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
